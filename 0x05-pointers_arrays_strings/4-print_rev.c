6a799010e2ff2998d9de72a6b415a5f779e6dd2a
Astawesegn <fikremati@gmail.com>
2022-12-19 06:16:51 -0800

    Reverse the String

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse the string
+ * @s: string
+ * return: 0
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+	int l;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	s--;
+	for (l = length; l > 0; l--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
