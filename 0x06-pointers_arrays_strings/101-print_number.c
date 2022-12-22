d3f006cda3ede0cc46dec17cee5f9643ec78ee5f
Astawesegn <fikremati@gmail.com>
2022-12-22 12:47:04 -0800

    Prints Integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_number - print numbers chars
+ * @n: integer params
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int j;
+
+	j = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		j = -n;
+	}
+	if (j / 10 != 0)
+	{
+		print_number(j / 10);
+	}
+	_putchar((j % 10) + '0');
+}
